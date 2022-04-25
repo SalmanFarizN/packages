79055f7621868cf2f8dd0b5af3e70c1b9908e858
SalmanFarizN <salmanfarizn@gmail.com>
2022-04-25 17:17:11 +0200

    updates to detect whether its trajectory or single snapshot

--- md_dat_analysis/md_dat_analysis/visualization.py
+++ md_dat_analysis/md_dat_analysis/visualization.py
@@ -7,15 +7,29 @@ def ovito2d(pos,colour2,path,filename):
     os.chdir(path)
     file = open(filename,'w')
 
-    for i in range(0,pos.shape[2]-1):
+    #3D array: trajectory file
+    if (len(pos.shape)==3):
+        for i in range(0,pos.shape[2]-1):
+            file.write('{}\n\n'.format(pos.shape[0]))
+            for j in range(pos.shape[0]):
+            
+                if (j in colour2):
+                    file.write('2 {} {}\n'.format(pos[j,0,i],pos[j,1,i]))
+            
+                else:
+                    file.write('1 {} {}\n'.format(pos[j,0,i],pos[j,1,i]))
+                    
+    
+    #2D array: Only a single frame
+    if (len(pos.shape)==2):
         file.write('{}\n\n'.format(pos.shape[0]))
         for j in range(pos.shape[0]):
             
             if (j in colour2):
-                file.write('2 {} {}\n'.format(pos[j,0,i],pos[j,1,i]))
+                file.write('2 {} {}\n'.format(pos[j,0],pos[j,1]))
             
             else:
-                file.write('1 {} {}\n'.format(pos[j,0,i],pos[j,1,i]))
+                file.write('1 {} {}\n'.format(pos[j,0],pos[j,1]))
 
     
     
