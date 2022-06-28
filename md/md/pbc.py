20a9b3cc651c20cae568bc00c265750ea49cfc31
SalmanFarizN <salmanfarizn@gmail.com>
2022-06-28 15:31:30 +0200

    added pbc wallhit check to update unfolded tracker

--- md/md/pbc.py
+++ md/md/pbc.py
@@ -14,6 +14,28 @@ def wallhit(nparticles,ndims,pos,length):
     return(pos)
 
 
+
+
+
+# Wall hit check with unfolding trackers 
+# : Entire snapshot array passed in check if any particle is touching the boundary
+@jit(nopython=True)
+def wallhit_unf(nparticles,ndims,pos,length,tr):
+    for i in range(nparticles):
+        for j in range(ndims):
+            if pos[i,j]< 0.0:
+                pos[i,j]+=length
+                tr[i,j]-=1
+                
+            if pos[i,j]>=length:
+                pos[i,j]-=length
+                tr[i,j]+=1
+    return(pos,tr)
+
+
+
+
+
 # distance computation b/w any 2 particles using the minimum image criteria: pass in co-ords of 2 particles 
 @jit(nopython=True)
 def dist_mic(ndims,pos1,pos2,length,hlength):
