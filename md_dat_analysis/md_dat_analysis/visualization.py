7fab52f05f327d8936c557c3d664b0f49074c626
SalmanFarizN <salmanfarizn@gmail.com>
2022-08-17 12:41:15 +0200

    added extended .xyz support (fix bounds)

--- md_dat_analysis/md_dat_analysis/visualization.py
+++ md_dat_analysis/md_dat_analysis/visualization.py
@@ -3,7 +3,7 @@ from md_dat_analysis import cluster_analysis as cla
 
 
 #Prepare xyz file to Visualize 2D particle system snapshots in OVITO
-def ovito2d(pos,colour2,path,filename):
+def ovito2d(pos,colour2,path,filename,length):
     
     os.chdir(path)
     file = open(filename,'w')
@@ -11,7 +11,8 @@ def ovito2d(pos,colour2,path,filename):
     #3D array: trajectory file
     if (len(pos.shape)==3):
         for i in range(0,pos.shape[2]-1):
-            file.write('{}\n\n'.format(pos.shape[0]))
+            file.write('{}\n'.format(pos.shape[0]))
+            file.write('Lattice="{} 0.0 0.0 0.0 {} 0.0 0.0 0.0 {}"\n'.format(length,length,length))
             for j in range(pos.shape[0]):
             
                 if (j in colour2):
@@ -23,7 +24,8 @@ def ovito2d(pos,colour2,path,filename):
     
     #2D array: Only a single frame
     if (len(pos.shape)==2):
-        file.write('{}\n\n'.format(pos.shape[0]))
+        file.write('{}\n'.format(pos.shape[0]))
+        file.write('Lattice="{} 0.0 0.0 0.0 {} 0.0 0.0 0.0 {}"\n'.format(length,length,length))
         for j in range(pos.shape[0]):
             
             if (j in colour2):
@@ -50,7 +52,8 @@ def ovito2d_abop(pos,path,filename,nparticles,ndims,sigma,length,hlength):
     phi4_avg,phi4=cla.abop(nparticles,ndims,4,r_list,nc)    
     phi6_avg,phi6=cla.abop(nparticles,ndims,6,r_list,nc)
     
-    file.write('{}\n\n'.format(pos.shape[0]))
+    file.write('{}\n'.format(pos.shape[0]))
+    file.write('Lattice="{} 0.0 0.0 0.0 {} 0.0 0.0 0.0 {}"\n'.format(length,length,length))
     for i in range(pos.shape[0]):
             
             
