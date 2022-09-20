1aacb49e5367b80b4455b29329faf16e033f1c21
SalmanFarizN <salmanfarizn@gmail.com>
2022-09-20 11:41:23 +0200

    abop coded videos and 3D sim vis

--- md_dat_analysis/md_dat_analysis/visualization.py
+++ md_dat_analysis/md_dat_analysis/visualization.py
@@ -38,6 +38,43 @@ def ovito2d(pos,colour2,path,filename,length):
     
     file.close()
     
+    
+    
+#Prepare xyz file to Visualize 3D particle system snapshots in OVITO
+def ovito3d(pos,colour2,path,filename,length):
+    
+    os.chdir(path)
+    file = open(filename,'w')
+
+    #3D array: trajectory file
+    if (len(pos.shape)==3):
+        for i in range(0,pos.shape[2]-1):
+            file.write('{}\n'.format(pos.shape[0]))
+            file.write('Lattice="{} 0.0 0.0 0.0 {} 0.0 0.0 0.0 {}"\n'.format(length,length,length))
+            for j in range(pos.shape[0]):
+            
+                if (j in colour2):
+                    file.write('2 {} {} {}\n'.format(pos[j,0,i],pos[j,1,i],pos[j,2,i]))
+            
+                else:
+                    file.write('1 {} {} {}\n'.format(pos[j,0,i],pos[j,1,i],pos[j,2,i]))
+                    
+    
+    #2D array: Only a single frame
+    if (len(pos.shape)==2):
+        file.write('{}\n'.format(pos.shape[0]))
+        file.write('Lattice="{} 0.0 0.0 0.0 {} 0.0 0.0 0.0 {}"\n'.format(length,length,length))
+        for j in range(pos.shape[0]):
+            
+            if (j in colour2):
+                file.write('2 {} {} {}\n'.format(pos[j,0],pos[j,1],pos[j,2]))
+            
+            else:
+                file.write('1 {} {} {}\n'.format(pos[j,0],pos[j,1],pos[j,2]))
+
+    
+    
+    file.close()
 
 
 
@@ -48,30 +85,67 @@ def ovito2d_abop(pos,path,filename,nparticles,ndims,sigma,length,hlength):
     
     file = open(filename,'w')
     
-    z,nc,nclist,r_list =cla.coord_number(nparticles,ndims,sigma,length,hlength,pos)
-    phi4_avg,phi4=cla.abop(nparticles,ndims,4,r_list,nc)    
-    phi6_avg,phi6=cla.abop(nparticles,ndims,6,r_list,nc)
+
     
-    file.write('{}\n'.format(pos.shape[0]))
-    file.write('Lattice="{} 0.0 0.0 0.0 {} 0.0 0.0 0.0 {}"\n'.format(length,length,length))
-    for i in range(pos.shape[0]):
+
+    
+    #2D array: Only a single frame
+    if (len(pos.shape)==2):
+        
+        z,nc,nclist,r_list =cla.coord_number(nparticles,ndims,sigma,length,hlength,pos)
+        phi4_avg,phi4=cla.abop(nparticles,ndims,4,r_list,nc)    
+        phi6_avg,phi6=cla.abop(nparticles,ndims,6,r_list,nc)
+        file.write('{}\n'.format(pos.shape[0]))
+        file.write('Lattice="{} 0.0 0.0 0.0 {} 0.0 0.0 0.0 {}"\n'.format(length,length,length))
+        
+        for i in range(pos.shape[0]):
             
             
-        #Fluid Condition
-        if (nc[i]<=2):
-            file.write('1 {} {}\n'.format(pos[i,0],pos[i,1]))
+            #Fluid Condition
+            if (nc[i]<=2):
+                file.write('1 {} {}\n'.format(pos[i,0],pos[i,1]))
 
-        #HCP Condition 
-        elif ((phi6[i]>=0.7)&(phi4[i]<=0.3)):
-            file.write('2 {} {}\n'.format(pos[i,0],pos[i,1]))
+            #HCP Condition 
+            elif ((phi6[i]>=0.7)&(phi4[i]<=0.3)):
+                file.write('2 {} {}\n'.format(pos[i,0],pos[i,1]))
 
-        #Quadratic Condition
-        elif ((phi4[i]>=0.7)&(phi6[i]<=0.3)):
-            file.write('3 {} {}\n'.format(pos[i,0],pos[i,1]))
+            #Quadratic Condition
+            elif ((phi4[i]>=0.7)&(phi6[i]<=0.3)):
+                file.write('3 {} {}\n'.format(pos[i,0],pos[i,1]))
 
-        #None above then disordered
-        else:
-            file.write('4 {} {}\n'.format(pos[i,0],pos[i,1]))
+            #None above then disordered
+            else:
+                file.write('4 {} {}\n'.format(pos[i,0],pos[i,1]))
+                
+                
+    if (len(pos.shape)==3):
+        for i in range(0,pos.shape[2]-1,100):
+            
+            z,nc,nclist,r_list =cla.coord_number(nparticles,ndims,sigma,length,hlength,pos[:,:,i])
+            phi4_avg,phi4=cla.abop(nparticles,ndims,4,r_list,nc)    
+            phi6_avg,phi6=cla.abop(nparticles,ndims,6,r_list,nc)
+            
+            file.write('{}\n'.format(pos.shape[0]))
+            file.write('Lattice="{} 0.0 0.0 0.0 {} 0.0 0.0 0.0 {}"\n'.format(length,length,length))
+        
+            for j in range(pos.shape[0]):
+            
+            
+                #Fluid Condition
+                if (nc[j]<=2):
+                    file.write('1 {} {}\n'.format(pos[j,0,i],pos[j,1,i]))
+
+                #HCP Condition 
+                elif ((phi6[j]>=0.7)&(phi4[j]<=0.3)):
+                    file.write('2 {} {}\n'.format(pos[j,0,i],pos[j,1,i]))
+
+                #Quadratic Condition
+                elif ((phi4[j]>=0.7)&(phi6[j]<=0.3)):
+                    file.write('3 {} {}\n'.format(pos[j,0,i],pos[j,1,i]))
+
+                #None above then disordered
+                else:
+                    file.write('4 {} {}\n'.format(pos[j,0,i],pos[j,1,i]))
             
     
     file.close()
