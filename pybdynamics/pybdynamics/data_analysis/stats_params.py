33cef8eb0701c01e8d8547e3dd0b7564fab8138f
SalmanFarizN <salmanfarizn@gmail.com>
2024-12-07 12:35:44 +0100

    Radial distribution functions: angle dependent, time averaged

--- pybdynamics/pybdynamics/data_analysis/stats_params.py
+++ pybdynamics/pybdynamics/data_analysis/stats_params.py
@@ -5,7 +5,7 @@ from pybdynamics import pbc
 
 # Radial distrubution function calculation
 @jit(nopython=True)
-def rdf(pos, length, rho, rmax, bins, dr, t_interval):
+def rdf_old(pos, length, rho, rmax, bins, dr, t_interval):
     hlength = length / 2.0
     r = np.linspace(0, rmax, bins)
     gr = np.zeros((len(r)), dtype=np.float32)
@@ -40,6 +40,149 @@ def rdf(pos, length, rho, rmax, bins, dr, t_interval):
     return (gr, r)
 
 
+# Function to calculate the distance between all pairs of particles
+# in a given frame t
+@jit(nopython=True)
+def dist_all_pairs(pos, length, hlength, t):
+    ndims = pos.shape[1]
+    nparticles = pos.shape[0]
+    spoints = pos.shape[2]
+
+    distarr = np.full((nparticles * nparticles), np.nan)
+    k = 0
+    for i in range(nparticles):
+        for j in range(nparticles):
+            if i != j:
+                dnorm, d = pbc.dist_mic(
+                    ndims, pos[i, :, t], pos[j, :, t], length, hlength
+                )
+                distarr[k] = dnorm
+                k = k + 1
+
+    return distarr
+
+
+# Function to calculate angle between all pairs of particles
+# in a given frame t (and also the distance between them)
+@jit(nopython=True)
+def angle_all_pairs(pos, length, hlength, t):
+    ndims = pos.shape[1]
+    nparticles = pos.shape[0]
+    spoints = pos.shape[2]
+
+    anglearr = np.full((nparticles * nparticles), np.nan)
+    distarr = np.full((nparticles * nparticles), np.nan)
+    k = 0
+    for i in range(nparticles):
+        for j in range(nparticles):
+            if i != j:
+                dnorm, d = pbc.dist_mic(
+                    ndims, pos[i, :, t], pos[j, :, t], length, hlength
+                )
+
+                if d[1] >= 0:
+                    anglearr[k] = np.arccos(d[0] / dnorm)
+                if d[1] < 0:
+                    anglearr[k] = 2 * np.pi - np.arccos(d[0] / dnorm)
+
+                # anglearr[k] = np.arccos(d[0] / dnorm)
+                distarr[k] = dnorm
+                k = k + 1
+
+    return distarr, anglearr
+
+
+# Radial distrubution function of a single frame without time averaging
+# but averaging over all particles
+
+
+def rdf(pos, length, rho, rmax, bins, t):
+    hlength = length / 2.0
+    ndims = pos.shape[1]
+    nparticles = pos.shape[0]
+    spoints = pos.shape[2]
+    # Calculate the distances between all pairs of particles
+    distarr = dist_all_pairs(pos, length, hlength, t)
+
+    # Calculate the radial distribution function using histogram
+    # of the distances between all pairs of particles
+    gr, r = np.histogram(distarr, bins=bins, range=(0, rmax), density=False)
+    r_centers = (r[1:] + r[:-1]) / 2
+    dr = r[1] - r[0]
+
+    for i in range(len(r_centers)):
+        gr[i] = gr[i] / (np.pi * ((r[i] + dr) ** 2 - r[i] ** 2) * rho)
+
+    return (gr, r_centers)
+
+
+# Angle dependent radial distrubution function of a single frame without time averaging
+# but averaging over all particles
+def adrdf(pos, length, rho, rmax, rbins, amax, abins, t):
+    hlength = length / 2.0
+    ndims = pos.shape[1]
+    nparticles = pos.shape[0]
+    spoints = pos.shape[2]
+
+    # Caalculate the distances and angles between all pairs of particles
+    distarr, anglearr = angle_all_pairs(pos, length, hlength, t)
+
+    # Calculate the 2D histogram of the angle dependent RDF
+    gr, r, a = np.histogram2d(
+        distarr,
+        anglearr,
+        bins=(rbins, abins),
+        range=((0, rmax), (0, amax)),
+        density=False,
+    )
+
+    r_centers = (r[1:] + r[:-1]) / 2
+    a_centers = (a[1:] + a[:-1]) / 2
+    dr = r[1] - r[0]
+    da = a[1] - a[0]
+
+    for i in range(len(r_centers)):
+        gr[i] = gr[i] / (np.pi * ((r[i] + dr) ** 2 - r[i] ** 2) * da * rho)
+
+    return (gr, r_centers, a_centers)
+
+
+# Time averaged angle dependant radial distrubution function
+def adrdf_tavg(pos, length, rho, rmax, rbins, amax, abins, tstart, tend):
+    ndims = pos.shape[1]
+    nparticles = pos.shape[0]
+    spoints = pos.shape[2]
+
+    for i in range(tstart, tend):
+        gr, r, a = adrdf(pos, length, rho, rmax, rbins, amax, abins, i)
+        if i == tstart:
+            gr_tavg = gr
+        else:
+            gr_tavg = gr_tavg + gr
+
+    gr_tavg = gr_tavg / (tend - tstart)
+
+    return (gr_tavg, r, a)
+
+
+# Time avergaed radial distrubution function
+def rdf_tavg(pos, length, rho, rmax, bins, tstart, tend):
+    ndims = pos.shape[1]
+    nparticles = pos.shape[0]
+    spoints = pos.shape[2]
+
+    for i in range(tstart, tend):
+        gr, r = rdf(pos, length, rho, rmax, bins, i)
+        if i == tstart:
+            gr_tavg = gr
+        else:
+            gr_tavg = gr_tavg + gr
+
+    gr_tavg = gr_tavg / (tend - tstart)
+
+    return (gr_tavg, r)
+
+
 # Calculate the unfolded position co ordinates of each particle's trajectory
 # for MSD calulcations
 @jit(nopython=True)
@@ -72,9 +215,44 @@ def msd(pos_unf, sigma, saved):
             dnorm, r = pbc.dist(ndims, pos_unf[j, :, i], pos_unf[j, :, 0])
             disp[j] = disp[j] + dnorm**2
 
-        msd[i] = (
-            np.sum(disp * sigma**2) / nparticles
-        )  # Dimension of disp^2 is sigma^2
+        msd[i] = np.sum(disp * sigma**2) / nparticles  # Dimension of disp^2 is sigma^2
+
+    return (msd, taus)
+
+
+# Calculate MSD (with time windowing)
+@jit(nopython=True)
+def msd_tw(pos_unf, sigma, saved):
+    nparticles = pos_unf.shape[0]
+    ndims = pos_unf.shape[1]
+    spoints = pos_unf.shape[2]
+
+    msd = np.zeros((spoints), dtype=np.float64)
+    taus = np.zeros((spoints), dtype=np.float64)
+
+    # loop over different lag-times i
+    for i in range(1, spoints):
+        taus[i] = i * 1e-5 * saved  # 1e-5 tau_B is the timestep
+        tw = 0
+        # Loop over all possible time windows
+        while i + tw < spoints:
+            if tw >= 5:
+                break
+            disp = np.zeros((nparticles), dtype=np.float64)
+            # Compute the displacement of each particle in the time window
+            for j in range(nparticles):
+                dnorm, r = pbc.dist(ndims, pos_unf[j, :, i + tw], pos_unf[j, :, tw])
+                disp[j] = disp[j] + dnorm**2
+            # Add the MSD of each time window to the total MSD at that lag-time i
+            msd[i] = msd[i] + (
+                np.sum(disp * sigma**2) / nparticles
+            )  # Dimension of disp^2 is sigma^2
+            # Update the time window
+            tw += 1
+
+        # Normalize the MSD by the number of time windows if it is non-zero
+        if tw != 0:
+            msd[i] = msd[i] / tw
 
     return (msd, taus)
 
@@ -102,7 +280,7 @@ def bij(pos, length, hlength, sigma):
 # Function to compute the bond autocorrelation funciton
 # 2 particles are defined as being bonded if they are nearest neighbours
 @jit(nopython=True)
-def bond_ac(pos, length, hlength, sigma):
+def bond_ac(pos, length, hlength, sigma, saved):
     nparticles = pos.shape[0]
     ndims = pos.shape[1]
     spoints = pos.shape[2]
@@ -110,10 +288,86 @@ def bond_ac(pos, length, hlength, sigma):
     bac = np.zeros((spoints), dtype=np.float64)
     taus = np.zeros((spoints), dtype=np.float64)
 
+    b_ij0 = bij(pos[:, :, 0], length, hlength, sigma)
+    # Flatten 2D array to 1D array
+    b_ij0 = b_ij0.flatten()
+    normalization = np.sum(b_ij0)
+
     for t in range(1, spoints):
-        taus[t] = t * 1e-5
-        b_ij0 = bij(pos[:, :, t - 1], length, hlength, sigma)
+        taus[t] = t * 1e-5 * saved
         b_ij = bij(pos[:, :, t], length, hlength, sigma)
-        bac[t] = np.sum(b_ij0 * b_ij) / np.sum(b_ij0)
+        # Flatten 2D array to 1D arrays
+        b_ij = b_ij.flatten()
+
+        bac[t] = np.sum(b_ij0 * b_ij) / normalization
 
     return bac, taus
+
+
+# Function to compute the time windowed bond autocorrelation function
+# 2 particles are defined as being bonded if they are nearest neighbours
+@jit(nopython=True)
+def bond_ac_tw(pos, length, hlength, sigma, saved):
+    nparticles = pos.shape[0]
+    ndims = pos.shape[1]
+    spoints = pos.shape[2]
+
+    bac = np.zeros((spoints), dtype=np.float64)
+    taus = np.zeros((spoints), dtype=np.float64)
+
+    # Loop over different lag-times i
+    for i in range(1, spoints):
+        taus[i] = i * 1e-5 * saved
+        tw = 0
+        # Loop over all possible time windows
+        while i + tw < spoints:
+            if tw >= 5:
+                break
+            b_ij0 = bij(pos[:, :, tw], length, hlength, sigma)
+            b_ij = bij(pos[:, :, i + tw], length, hlength, sigma)
+            # Flatten both 2D arrays to 1D arrays
+            b_ij0 = b_ij0.flatten()
+            b_ij = b_ij.flatten()
+
+            bac[i] = bac[i] + np.sum(b_ij0 * b_ij) / np.sum(b_ij0)
+            tw += 1
+
+        # Normalize the bond autocorrelation by the number of time windows if it is non-zero
+        if tw != 0:
+            bac[i] = bac[i] / tw
+
+    return bac, taus
+
+
+# Fucntion to compute the particle level entropy (entropy produced by the particle due to heat transported
+# by the particle into the bath) using Stratanovic integration rules
+@jit(nopython=True)
+def entropy_strat(pos_prev, pos, pos_new, force, kBT, step_size, length, hlength):
+    nparticles = pos.shape[0]
+    ndims = pos.shape[1]
+    force_scalar = np.zeros((nparticles), dtype=np.float64)
+    disp_vec = np.zeros((nparticles, ndims), dtype=np.float64)
+    disp_scalar = np.zeros((nparticles), dtype=np.float64)
+    delta_q = np.zeros((nparticles), dtype=np.float64)
+    entropy_prod = np.zeros((nparticles), dtype=np.float64)
+
+    # Caluclate the displacement 2D vector b/w pos_prev and pos_new
+    # using mimimum image criteria
+    for i in range(nparticles):
+        dnorm, disp_vec[i, :] = pbc.dist_mic(
+            ndims, pos_new[i, :], pos_prev[i, :], length, hlength
+        )
+        disp_scalar[i] = dnorm
+
+    disp_vec = (disp_vec) / (2.0 * step_size)
+
+    # Calculate heat produced by each particle into the bath
+    # Q_dot = Force * (r(t+dt)-r(t-dt))/2dt
+    for i in range(nparticles):
+        delta_q[i] = np.dot(force[i, :], disp_vec[i, :])
+
+    # Calculate the entropy produced by each particle into the bath
+    # S_dot = delta_q/T
+    entropy_prod = delta_q / kBT
+
+    return entropy_prod
